4ec1370ae1eda563be7fcd58e02e5f8142aa4b0e
Zacky Viriot <zackyviriot@Zackys-MBP.attlocal.net>
2022-07-01 01:21:30 -0500

    added authroization to users

--- src/Validation/registerValidation.js
+++ src/Validation/registerValidation.js
@@ -1,44 +1,38 @@
 const Validator = require('validator');
 const isEmpty = require('./isEmpty')
 
-
 const validateRegisterInput = (data) => {
     let errors = {};
-
-    // check the email field 
+    //check the email field 
     if(isEmpty(data.email)){
-        errors.email = "Email field cannot be empty"
-    }else if (!Validator.isEmail(data.email)){
-        errors.email = 'Email is invalid, please provide a valid email'
+        errors.email = 'Email field cannot be empty'
+    } else if (!Validator.isEmail(data.email)){
+        errors.email = 'Email is invalid please enter a vlid email'
     }
-     // Check Name field
-     if(isEmpty(data.name)){
-        errors.name = 'Name field can not be empty';
-    }else if (!Validator.isLength(data.name,{min:2,max:30})){
-        errors.name = 'Name must be 2 to 30 characters long'
-    }
-
-    // Check Password field
+    //check password field 
     if(isEmpty(data.password)){
-        errors.password = 'Password field cannot be empty';
+        errors.password = 'Password field cannot be empty'
     }else if (!Validator.isLength(data.password,{min:6,max:150})){
         errors.password = 'Password must be between 6 and 150 characters long'
     }
-
-    //Check confirm password field 
-    if (isEmpty(data.confirmPassword)){
+    //check name field 
+    if(isEmpty(data.name)){
+        errors.name = 'name field cannot be empty'
+    }else if (!Validator.isLength(data.name,{min:2,max:30})){
+        errors.name = 'name must be between 2 and 30 characters long'
+    }
+    // check confirm password field 
+    if(isEmpty(data.confirmPassword)){
         errors.confirmPassword = 'Confirm Password field can not be empty'
-    }else if (!Validator.equals(data.password, data.confirmPassword)){
-        errors.confirmPassword = 'Password and Confirm Password fields must match'
+    }else if(!Validator.equals(data.password,data.confirmPassword)){
+        errors.confirmPassword = 'Password and Confirm Password field must be the same'
     }
 
     return {
         errors,
         isValid: isEmpty(errors),
 
-
-
     }
-
 }
-module.exports = validateRegisterInput
+
+module.exports = validateRegisterInput;
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const User = require('../models/User')
 const bcrypt = require('bcryptjs')
 const validateRegisterInput = require('../Validation/registerValidation')
+const jwt = require('jsonwebtoken')
+const requiresAuth = require("../middleware/permission")
 
 // @route       GET /api/auth/test
 // @desc        Test the auth route
@@ -39,11 +41,15 @@ router.post("/register", async (req,res) => {
             name:req.body.name
         })
 
-        // save the user to data base 
+        //save the user to databse 
         const savedUser = await newUser.save()
 
+        const userToReturn = {...savedUser._doc}
+        delete userToReturn.password
+
+
         //retrun the new user 
-        return res.json(savedUser)
+        return res.json(userToReturn)
 
     } catch (err) {
         //Error here
@@ -52,7 +58,62 @@ router.post("/register", async (req,res) => {
         res.status(500).send(err.message)
     }
 })
+// @route       POST/api/auth/login
+// @desc        Login user and return a access token
+// @access      Public
+
+router.post('/login',async (req,res) => {
+    try {
+        // check for the user 
+        const user = await User.findOne({
+            email:new RegExp("^" + req.body.email + "$", "i")
+        })
+        if(!user){
+            return res.status(400).json({error:'There was a problem with your login credentials'})
+        }
 
 
+        const passwordMatch = await bcrypt.compare(req.body.password,user.password);
+
+        if(!passwordMatch){
+            return res.status(400).json({error:'There was a problem with your login credentials'})
+        }
+        const payload = {userId: user._id};
+        const token = jwt.sign(payload,process.env.JWT_SECRET, {
+            expiresIn: "7d"
+        })
+
+        res.cookie("access-token",token,{
+            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 100),
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production'
+        });
+        const userToReturn = {...user._doc};
+        delete userToReturn.password;
+        return res.json({
+            token:token,
+            user:userToReturn,
+
+        })
+
+        return res.json({passwordMatch:passwordMatch})
+    }catch(err){
+        console.log(err);
+
+        return res.status(500).send(err.message)
+    }
+})
+// @route       GET /api/auth/current
+// @desc        Return the currently authed user
+// @access      private
+
+router.get("/current", requiresAuth ,(req,res) => {
+    if(!req.user){
+        return res.status(401).send("Unauthorized")
+
+    }
+
+    return res.json(req.user)
+})
 
 module.exports = router;
