2251dc35fb898aff63dee5f78ccbce89db2254ba
Zacky Viriot <zackyviriot@Zackys-MBP.attlocal.net>
2022-07-07 02:51:34 -0500

    Finished API endpoints

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -116,4 +116,20 @@ router.get("/current", requiresAuth ,(req,res) => {
     return res.json(req.user)
 })
 
+// @ route  PUT/api/auth/logout
+// @  desc  Logout user and clear the cookie 
+// @ access private
+
+
+router.put("/logout",requiresAuth, async(req,res) => {
+    try{
+        res.clearCookie("access-token")
+
+
+        return res.json({success: true})
+    }catch(err){
+        console.log(err)
+        return res.status(500).send(err.message)
+    }
+})
 module.exports = router;
--- src/routes/ticket.js
+++ src/routes/ticket.js
@@ -67,7 +67,169 @@ router.get('/current',requiresAuth, async (req,res) => {
         return res.status(500).send(err.message)
     }
 })
+// @route PUT/api/ticket/:ticketId/complete
+// @desc Mark a ticket as complete
+// @access  Private 
+
+
+router.put('/:ticketId/complete',requiresAuth,async(req,res) => {
+    try{
+        const ticket = await Ticket.findOne({
+            user: req.user._id,
+            _id: req.params.ticketId
+        })
+        if(!ticket){
+            return res.status(404).json({error:'Could not find Ticket'})
+        }
+        if(ticket.complete){
+            res.status(400).json({error: 'Ticket is already complete'})
+        }
+
+        const updatedTicket = await Ticket.findOneAndUpdate(
+            {
+                user: req.user._id,
+                _id:req.params.ticketId
+            },
+            {
+                complete:true,
+                completedAt: new Date(),
+            },
+            {
+                new: true
+            }
+
+        )
+        return res.json(updatedTicket);
+    }catch(err){
+        console.log(err);
+        return res.status(500).send(err.message)
+    }
+})
+
+// @route PUT/api/ticket/:ticketId/incomplete
+// @desc Mark a ticket as incomplete
+// @access  Private 
+
+router.put("/:ticketId/incomplete",requiresAuth , async (req,res) => {
+    try{
+        const ticket = await Ticket.findOne({
+            user:req.user._id,
+            _id: req.params.ticketId
+        })
+
+        if(!ticket){
+            return res.status(404).json({error: "Could not find ticket"})
+        }
+
+        if(!ticket.complete){
+            return res.status(400).json({error: "Ticket is already incomplete"})
+        }
+
+        const updatedTicket = await Ticket.findOneAndUpdate(
+            {
+                user:req.user._id,
+                _id: req.params.ticketId,
+            },
+            {
+                complete:false,
+                completedAt:null
+            },
+            {
+                new:true,
+            }
+        )
+        return res.json(updatedTicket)
+    }catch(err){
+        console.log(err);
+        return res.status(500).send(err.message)
+    }
+})
+
+
+// @ route PUT/api/ticket/:ticketId
+// @  desc update a ticket
+// @ access private
+
+router.put("/:ticketId",requiresAuth, async (req,res) => {
+    try{
+        const ticket  = await Ticket.findOne({
+            user:req.user._id,
+            _id: req.params.ticketId
+        })
+
+        if(!ticket){
+            return res.status(404).json({error: "Could not find ticket"})
+        }
+        const {isValid,errors} = validateTicketInput(req.body);
+
+        if(!isValid){
+            return res.status(400).json(errors)
+        }
+
+        const updatedTicket = await Ticket.findOneAndUpdate(
+            {
+                user:req.user._id,
+                _id:req.params.ticketId ,
+            },
+            {
+                content: req.body.content
+            },
+            {
+                new:true
+            }
+        )
+        return res.json(updatedTicket)
+
+    }catch(err){
+        console.log(err)
+
+        return res.status(500).send(err.message)
+    }
+})
 
 
 
+// @ route  Delete/api/ticket/:ticketId
+// @  desc Delete a ticket
+// @ access private
+
+
+router.delete("/:ticketId",requiresAuth, async (req,res )=> {
+    try{
+        const ticket = await Ticket.findOne({
+            user: req.user._id,
+            _id:req.params.ticketId
+        });
+        if(!ticket){
+            return res.status(404).json({error: "Could not find ticket"})
+        }
+
+        await Ticket.findOneAndRemove({
+            user: req.user._id,
+            _id: req.params.ticketId
+        })
+        return res.json({success:true});
+    }catch(err){
+        console.log(err)
+        return res.status(500).send(err.message)
+    }
+})
+
+
+// @ route  PUT/api/auth/logout
+// @  desc  Logout user and clear the cookie 
+// @ access private
+
+
+router.put("/logout",requiresAuth, async(req,res) => {
+    try{
+        res.clearCookie("access-token")
+
+
+        return res.json({success: true})
+    }catch(err){
+        console.log(err)
+        return res.status(500).send(err.message)
+    }
+})
 module.exports = router;
